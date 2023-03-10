e16d999aa8bd14f3ab292ee620dfb3a36a5c4c64
Dynex [DNX] <113523376+dynexcoin@users.noreply.github.com>
2023-03-10 07:56:26 +0100

    Add files via upload

--- src/PaymentGateService/PaymentGateService.cpp
+++ src/PaymentGateService/PaymentGateService.cpp
@@ -186,7 +186,7 @@ void PaymentGateService::runInProcess(Logging::LoggerRef& log) {
   log(Logging::INFO) << "Starting Payment Gate with local node";
 
   CryptoNote::Currency currency = currencyBuilder.currency();
-  CryptoNote::core core(currency, NULL, logger, false);
+  CryptoNote::core core(currency, NULL, logger, true);
 
   CryptoNote::CryptoNoteProtocolHandler protocol(currency, *dispatcher, core, NULL, logger);
   CryptoNote::NodeServer p2pNode(*dispatcher, protocol, logger);
